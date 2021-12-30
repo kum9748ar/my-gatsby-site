a731c0382db49d9bdd011a867492aa1532cb30d8
Kumar Kalyan <kum9748ar@gmail.com>
2021-12-30 14:11:12 +0530

    Modified the data

---
+++ src/styles/style.js
@@ -0,0 +1,80 @@
+const style = {
+    pageStyles: {
+        color: "#232129",
+        padding: 0,
+        fontFamily: " Roboto, sans-serif, serif",
+        fontWeight: 300
+    },
+    headingStyles: {
+        marginTop: 0,
+        marginBottom: 64,
+        maxWidth: 320,
+    },
+    headingAccentStyles: {
+        color: "#663399",
+    },
+    paragraphStyles: {
+        marginBottom: 10,
+    },
+    codeStyles: {
+        color: "#8A6534",
+        padding: 4,
+        backgroundColor: "#FFF4DB",
+        fontSize: "1.25rem",
+        borderRadius: 4,
+    },
+    listStyles: {
+        marginBottom: 96,
+        paddingLeft: 0,
+    },
+    listItemStyles: {
+        fontWeight: 300,
+        fontSize: 24,
+        maxWidth: 560,
+        marginBottom: 30,
+    },
+
+    docLinkStyle: {
+        listStyleType: "none",
+        marginBottom: 24,
+    },
+
+    descriptionStyle: {
+        color: "#232129",
+        fontSize: 14,
+        marginTop: 10,
+        marginBottom: 0,
+        lineHeight: 1.25,
+    },
+
+    docLink: {
+        text: "Documentation",
+        url: "https://www.gatsbyjs.com/docs/",
+        color: "#8954A8",
+    },
+
+    badgeStyle: {
+        color: "#fff",
+        backgroundColor: "#088413",
+        border: "1px solid #088413",
+        fontSize: 11,
+        fontWeight: "bold",
+        letterSpacing: 1,
+        borderRadius: 4,
+        padding: "4px 6px",
+        display: "inline-block",
+        position: "relative",
+        top: -2,
+        marginLeft: 10,
+        lineHeight: 1,
+    },
+    cardStyle: {
+        transition: 0.3,
+        borderRadius: 5,
+        img: {
+            borderRadius:"border-radius: 5px 5px 0 0",
+        }
+    }
+
+}
+export default style
